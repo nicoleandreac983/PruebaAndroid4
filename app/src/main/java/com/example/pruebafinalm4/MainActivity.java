4a71150c90c2414fd922c31a6b2d23e972ec1810
nicoleandreac983 <nicole_030790@hotmail.com>
2026-02-13 14:16:08 -0300

    App con primera pantalla

---
+++ app/src/main/java/com/example/pruebafinalm4/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pruebafinalm4;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText tvBienvenida;
+    private EditText tvSubtitulo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
